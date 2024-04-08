095b87425af27b339ac5ba906b8db3533ab6b9f4
Piyush Singh Negi <126347214+AdvancePlayer@users.noreply.github.com>
2024-04-08 12:24:17 +0530

    Update music.py

--- music.py
+++ music.py
@@ -30,23 +30,23 @@ def play_song():
     music_name.config(text=music_name[1:-4])
     
 root.overrideredirect()
-icon = "images/icon.ico"
+icon = "icon.ico"
 root.wm_iconbitmap(icon)
 
-image = Image.open("images/music player.png") 
+image = Image.open("music player.png") 
 photo1 = ImageTk.PhotoImage(image)
 bg_img = Label(root, image=photo1)
 bg_img.place(x=0, y=0, relwidth=1, relheight=1)
 
-play_icon=Image.open("images/play.png")
+play_icon=Image.open("play.png")
 photo2 = ImageTk.PhotoImage(play_icon)
 Button(root,image=photo2,border="0",command=play_song).place(x=150,y=420)
 
-resume_icon=Image.open("images/resume.png")
+resume_icon=Image.open("resume.png")
 photo4 = ImageTk.PhotoImage(resume_icon)
 Button(root,image=photo4,border="0",bg="#D9D9D9",command=mixer.music.unpause).place(x=200,y=530)
 
-pause_icon=Image.open("images/pause.png")
+pause_icon=Image.open("pause.png")
 photo3 = ImageTk.PhotoImage(pause_icon)
 Button(root,image=photo3,border="0",bg="#D9D9D9",command=mixer.music.pause).place(x=90,y=530)
 
